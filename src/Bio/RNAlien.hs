231cbe04004867d6b8b5ea67bb4c754d48cff19a
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-11-12 14:42:35 +0100

    Added prototype model construction function

--- src/Bio/RNAlien.hs
+++ src/Bio/RNAlien.hs
@@ -33,18 +33,41 @@ data Options = Options
     outputPath :: String
   } deriving (Show,Data,Typeable)
 
+-- | Keeps track of model construction 
+data ModelConstruction = ModelConstruction
+  { alignmentPath :: String,
+    modelPath :: String,
+    sessionID :: String,
+    iterationnumber :: Int
+  } deriving (Show) 
+
 options = Options
   { inputFile = def &= name "i" &= help "Path to input fasta file",
     outputPath = def &= name "o" &= help "Path to output directory"
   } &= summary "RNAlien devel version" &= help "Florian Eggenhofer - 2013" &= verbosity             
+
+-- | Initial RNA family model construction - generates iteration number, seed alignment and model
+modelConstruction :: String -> String -> IO ModelConstruction
+modelConstruction sessionID inputFasta = do
+  -- Iterationnumber 
+  let iterationNumber = 0
+  -- Blast for initial sequence set
+  blastoutput <- systemBlast inputFasta iterationNumber
+  let modelPath = "modelPath"
+  let alignmentPath = "alignmentPath"
+  return $ ModelConstruction modelPath alignmentPath sessionID iterationNumber
+
+-- | RNA family model expansion 
+--modelExpansion iterationnumber alignmentPath modelPath = do
 
 -- | Adds cm prefix to pseudo random number
 randomid :: Int16 -> String
 randomid number = "cm" ++ (show number)
 
 -- | Run external blast command and read the output into the corresponding datatype
+systemBlast :: String -> Int -> IO BlastResult
 systemBlast filepath iterationnumber = do
-  let outputName = iterationnumber ++ ".blastout"
+  let outputName = (show iterationnumber) ++ ".blastout"
   system ("blastn -outfmt 5 -query " ++ filepath  ++ " -db refseq_genomic -out " ++ outputName)
   inputBlast <- readXML outputName
   return inputBlast
@@ -67,20 +90,22 @@ systemCMcompare filepath iterationnumber = system ("CMcompare " ++ filepath ++ "
 main = do
   args <- getArgs
   Options{..} <- cmdArgs options       
-  input_present <- doesFileExist inputFile
-  output_present <- doesFileExist outputPath                   
+  --input_present <- doesFileExist inputFile
+  --output_present <- doesFileExist outputPath                   
 
-  let input_present_string = show input_present
-  let output_present_string = show output_present                          
+  --let input_present_string = show input_present
+  --let output_present_string = show output_present                          
 
--- | Create unique session id
-  randomnumber <- randomIO :: IO Int16
-  let sessionid = randomid randomnumber
-  
-  let filepath = "~egg/test.fa"
   -- read RNAz outputfile
-  rnazparsed <- parseRNAz inputFile
-  blastoutput <- systemBlast filepath "1"
-  print blastoutput
-  print rnazparsed           
-  print (randomid randomnumber)
+  --rnazparsed <- parseRNAz inputFile
+  --print rnazparsed    
+  --blastoutput <- systemBlast filepath "1"
+  
+   -- SessionID, iterationNumber
+  randomnumber <- randomIO :: IO Int16
+  let sessionId = randomid randomnumber
+  --create seed model
+  seedModel <- modelConstruction sessionId inputFile
+  print seedModel
+           
+
