b58ce859bc8865d91dd8e2e14e9ba7715bb3dd0c
Florian Eggenhofer <egg@tbi.univie.ac.at>
2014-07-13 17:42:50 +0200

    Added RfamStat to cabal file and added grouping of input Rfam fasta according to Family index number.

--- src/Bio/RfamStat.hs
+++ src/Bio/RfamStat.hs
@@ -10,6 +10,7 @@ import Text.ParserCombinators.Parsec
 import System.IO
 import System.Environment
 import System.Directory
+import Data.List.Split
 import Data.List
 import Data.Char
 import Bio.Core.Sequence 
@@ -19,21 +20,27 @@ import Bio.ClustalParser
 import System.Directory
 import System.Cmd   
 import qualified Data.Vector as V
+import qualified Data.ByteString.Lazy.Char8 as L
 data Options = Options            
   { inputFastaFilePath :: String,
-    outputPath :: String,
+    outputPath :: String
   } deriving (Show,Data,Typeable)
 
 options = Options
   { inputFastaFilePath = def &= name "i" &= help "Path to input fasta file",
-    outputPath = def &= name "o" &= help "Path to output directory",
+    outputPath = def &= name "o" &= help "Path to output directory"
   } &= summary "RfamStat" &= help "Florian Eggenhofer - 2014" &= verbosity       
-
 main = do
   args <- getArgs
   Options{..} <- cmdArgs options       
-  createDirectory (tempDirPath)
+  --createDirectory (outputPath)
   inputFasta <- readFasta inputFastaFilePath
-  print "test"
+  let rfamGroupedByIndex = groupByRfamIndex inputFasta
+  print (head rfamGroupedByIndex)
   
+groupByRfamIndex :: [Sequence] -> [[Sequence]] 
+groupByRfamIndex inputFasta = groupBy sameRfamIndex inputFasta
 
+sameRfamIndex sequence1 sequence2 =  (extractRfamIndex sequence1) ==  (extractRfamIndex sequence2)
+
+extractRfamIndex sequence =  head (splitOn ";" (L.unpack (unSL (seqid sequence))))
