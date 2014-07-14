3cd9a166c628f40612e3575506d56b074c7ae842
Florian Eggenhofer <egg@tbi.univie.ac.at>
2014-07-14 17:04:08 +0200

    Added alignment and SCI computation to RfamStat

--- src/Bio/RfamStat.hs
+++ src/Bio/RfamStat.hs
@@ -21,6 +21,9 @@ import System.Directory
 import System.Cmd   
 import qualified Data.Vector as V
 import qualified Data.ByteString.Lazy.Char8 as L
+import Data.Either
+import Data.Either.Unwrap
+import Bio.RNAlienLibary
 data Options = Options            
   { inputFastaFilePath :: String,
     outputPath :: String
@@ -38,8 +41,46 @@ main = do
   let rfamFamilies = groupByRfamIndex inputFasta
   let rfamIndexedFamilies = (V.indexed (V.fromList rfamFamilies))
   V.mapM_ (\(number,sequence) -> writeFasta (outputPath ++ (show number) ++ ".fa") sequence) rfamIndexedFamilies
-  --print (head rfamGroupedByIndex)
-  
+  let pairwiseFastaFilepath = constructPairwiseFastaFilePaths outputPath rfamIndexedFamilies
+  let pairwiseClustalw2Filepath = constructPairwiseAlignmentFilePaths "clustalw2" outputPath rfamIndexedFamilies
+  let pairwiseClustalw2SummaryFilepath = constructPairwiseAlignmentSummaryFilePaths outputPath rfamIndexedFamilies
+  let pairwiseLocarnaFilepath = constructPairwiseAlignmentFilePaths "mlocarna" outputPath rfamIndexedFamilies
+  let pairwiseLocarnainClustalw2FormatFilepath = constructPairwiseAlignmentFilePaths "mlocarnainclustalw2format" outputPath rfamIndexedFamilies
+  alignSequences "clustalw2" pairwiseFastaFilepath pairwiseClustalw2Filepath pairwiseClustalw2SummaryFilepath
+  alignSequences "mlocarna" pairwiseFastaFilepath pairwiseLocarnaFilepath []
+  --compute SCI
+  let pairwiseClustalw2RNAzFilePaths = constructPairwiseRNAzFilePaths "clustalw2" outputPath rfamIndexedFamilies
+  let pairwiseLocarnaRNAzFilePaths = constructPairwiseRNAzFilePaths "mlocarana" outputPath rfamIndexedFamilies
+  computeAlignmentSCIs pairwiseClustalw2Filepath pairwiseClustalw2RNAzFilePaths
+  computeAlignmentSCIs pairwiseLocarnainClustalw2FormatFilepath pairwiseLocarnaRNAzFilePaths
+  --retrieveAlignmentSCIs
+  clustalw2RNAzOutput <- mapM readRNAz pairwiseClustalw2RNAzFilePaths
+  mlocarnaRNAzOutput <- mapM readRNAz pairwiseLocarnaRNAzFilePaths 
+  let clustalw2SCI = map (\x -> (structureConservationIndex (fromRight x))) clustalw2RNAzOutput
+  let clustalw2SCIaverage = (sum clustalw2SCI) / 5 --(toRational (length clustalw2SCI))
+  let clustalw2SCImax = maximum clustalw2SCI
+  let clustalw2SCImin = minimum clustalw2SCI
+  let locarnaSCI = map (\x -> (structureConservationIndex (fromRight x))) mlocarnaRNAzOutput
+  let locarnaSCIaverage = (sum clustalw2SCI) / 5 --(toRational (length clustalw2SCI))
+  let locarnaSCImax = maximum locarnaSCI
+  let locarnaSCImin = minimum locarnaSCI
+  putStrLn "clustalw2averageSCI:"
+  putStrLn (show clustalw2SCIaverage)
+  putStrLn "clustalw2maxSCI:"
+  putStrLn (show clustalw2SCImax)
+  putStrLn "clustalw2minSCI:"
+  putStrLn (show clustalw2SCImin)
+  putStrLn "mlocarnaaverageSCI:"
+  putStrLn (show locarnaSCIaverage)
+  putStrLn "mlocarnamaxSCI:"
+  putStrLn (show locarnaSCImax)
+  putStrLn "mlocarnaminSCI:"
+  putStrLn (show locarnaSCImin)
+  putStrLn "clustalw2SCIs:"
+  print clustalw2SCI
+  putStrLn "mlocarnaminSCIs:"
+  print locarnaSCI
+
 groupByRfamIndex :: [Sequence] -> [[Sequence]] 
 groupByRfamIndex inputFasta = groupBy sameRfamIndex inputFasta
 
