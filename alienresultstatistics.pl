9c3f68fe6072c3c055c8669502bdf7241ffbe104
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-03-11 16:20:06 +0100

    Added tsv output for RNAlienstatistics script additional to verbose output and performed general cleanup

--- alienresultstatistics.pl
+++ alienresultstatistics.pl
@@ -3,19 +3,19 @@
 use warnings;
 use strict;
 use diagnostics;
-use utf8;
+#use utf8;
 use Data::Dumper;
 use Cwd;
 $|=1;
 
 my $counter = 1;
 #contains all RNAlien result folders for sRNA tagged families
-my $alienresult_basename="/scratch/egg/AlienTestResult4/temp/";
+my $alienresult_basename="/scratch/egg/AlienResultsCollected/";
 #contains all Rfam Families names by family name with extension .cm
 my $rfammodel_basename = "/scratch/egg/AlienTest/sRNAFamilies/all_models/";
 #contains all full seed alignment sequences as RfamID .fa fasta files
 my $rfamfasta_basename = "/scratch/egg/rfamfamilyfasta/";
-my $sRNAFamilyIdFile = "/scratch/egg/sRNAFamilyNameId";
+my $sRNAFamilyIdFile = "/scratch/egg/sRNAFamiliesIdNameGatheringCutoffTagSorted";
 my @sRNAfamilies;
 open(my $sRNAfamilyfh, "<", $sRNAFamilyIdFile)
     or die "Failed to open file: $!\n";
@@ -25,21 +25,30 @@ while(<$sRNAfamilyfh>) {
 }
 close $sRNAfamilyfh;
 
-for(1..1){
+for(1..374){
 	my $current_alienresult_folder= $alienresult_basename.$counter."/";
 	if(-e $alienresult_basename.$counter."/done"){
-		print "$counter\n";
 		my $alienModelPath = $current_alienresult_folder."result.cm";
 		my $alienFastaPath = $current_alienresult_folder."result.fasta";
-		my $alienThreshold = "80";
+                my $alienThresholdLogFile = $current_alienresult_folder."1.log";
+                my @alienThresholdLog;
+                open(my $alienThresholdLogfh, "<", $alienThresholdLogFile)
+                    or die "Failed to open file: $!\n";
+                while(<$alienThresholdLogfh>) {
+                    chomp;
+                    push @alienThresholdLog, $_;
+                }
+                close $sRNAfamilyfh;
+                my @alienThresholdLogSplit = split (/,/,$alienThresholdLog[0]);
+		my $alienThreshold = $alienThresholdLogSplit[2];
 		my @rfamModelNameId = split(/\s+/,$sRNAfamilies[($counter - 1)]);
-                my $rfamModelName = $rfamModelNameId[0];
-                my $rfamModelId = $rfamModelNameId[1];
-		my $rfamModelPath = $rfammodel_basename . $rfamModelName . ".cm";
+                my $rfamModelName = $rfamModelNameId[1];
+                my $rfamModelId = $rfamModelNameId[0];
+		my $rfamModelPath = $rfammodel_basename . $rfamModelId . ".cm";
                 my $rfamFastaPath =$rfamfasta_basename . $rfamModelId . ".fa";
-                my $rfamThreshold = "80";
-                print "RNAlienStatistics -i $alienModelPath -r $rfamModelPath -a $alienFastaPath -g $rfamFastaPath -o /scratch/egg/temp/AlienResultStatistics/";
-		`RNAlienStatistics -i $alienModelPath -r $rfamModelPath -a $alienFastaPath -g $rfamFastaPath -t $alienThreshold -x $rfamThreshold -o /scratch/egg/temp/AlienResultStatistics`;
+                my $rfamThreshold = $rfamModelNameId[2];
+                #print "RNAlienStatistics -i $alienModelPath -r $rfamModelPath -a $alienFastaPath -g $rfamFastaPath -o /scratch/egg/temp/AlienResultStatistics/";
+		print `RNAlienStatistics -n $rfamModelName -d $rfamModelId -b $counter -i $alienModelPath -r $rfamModelPath -a $alienFastaPath -g $rfamFastaPath -t $alienThreshold -x $rfamThreshold -o /scratch/egg/temp/AlienResultStatistics`;
 	}
 	$counter++;
 }
--- src/Bio/RNAlienStatistics.hs
+++ src/Bio/RNAlienStatistics.hs
@@ -6,24 +6,15 @@
 module Main where
     
 import System.Console.CmdArgs      
-import Data.Csv
-import Data.Maybe
-import Data.Either
 import Data.Either.Unwrap
-import qualified Data.Vector as V
 import System.Process
-import System.Exit
-import Control.Exception
 import qualified Data.ByteString.Lazy.Char8 as L
-import Data.Char
 import Bio.RNAlienLibrary
 import Bio.RNAlienData
 import System.Directory
-import Control.Monad
 import Bio.Core.Sequence 
 import Bio.Sequence.Fasta
 import Data.List
-import Data.List.Split
 import qualified System.FilePath as FP
 
 data Options = Options            
@@ -31,9 +22,12 @@ data Options = Options
     rfamCovarianceModelPath :: String,
     rfamFastaFilePath :: String,
     alienFastaFilePath :: String,
-    rfamThreshold :: Int,
-    alienThreshold :: Int,
+    rfamModelName :: String,
+    rfamModelId :: String,                     
+    rfamThreshold :: Double,
+    alienThreshold :: Double,
     outputDirectoryPath :: String,
+    benchmarkIndex :: Int,
     threads :: Int
   } deriving (Show,Data,Typeable)
 
@@ -42,27 +36,30 @@ options = Options
   { alienCovarianceModelPath = def &= name "i" &= help "Path to alienCovarianceModelPath",
     rfamCovarianceModelPath = def &= name "r" &= help "Path to rfamCovarianceModelPath",
     rfamFastaFilePath = def &= name "g" &= help "Path to rfamFastaFile",
+    rfamModelName = def &= name "n" &= help "Rfam model name",
+    rfamModelId = def &= name "d" &= help "Rfam model id",               
     alienFastaFilePath = def &= name "a" &= help "Path to alienFastaFile",
     outputDirectoryPath = def &= name "o" &= help "Path to output directory",
     alienThreshold = 20 &= name "t" &= help "Bitscore threshold for RNAlien model hits on Rfam fasta, default 20",
     rfamThreshold = 20 &= name "x" &= help "Bitscore threshold for Rfam model hits on Alien fasta, default 20",
+    benchmarkIndex = 1 &= name "b" &= help "Index used to identify sRNA tagged RNA families",
     threads = 1 &= name "c" &= help "Number of available cpu slots/cores, default 1"
   } &= summary "RNAlienStatistics devel version" &= help "Florian Eggenhofer - >2013" &= verbosity       
 
 --cmSearchFasta threads rfamCovarianceModelPath outputDirectoryPath "Rfam" False genomesDirectoryPath
-cmSearchFasta :: Int -> Int -> String -> String -> String -> String -> IO ([CMsearchHitScore],[CMsearchHitScore])
+cmSearchFasta :: Double -> Int -> String -> String -> String -> String -> IO ([CMsearchHitScore],[CMsearchHitScore])
 cmSearchFasta thresholdScore cpuThreads covarianceModelPath outputDirectory modelType fastapath = do
   createDirectoryIfMissing False (outputDirectory ++ "/" ++ modelType)
   let fastaFileName = FP.takeFileName fastapath
-  systemCMsearch cpuThreads covarianceModelPath fastapath (outputDirectory ++ "/" ++ modelType ++ "/" ++ fastaFileName ++ ".cmsearch")
+  _ <- systemCMsearch cpuThreads covarianceModelPath fastapath (outputDirectory ++ "/" ++ modelType ++ "/" ++ fastaFileName ++ ".cmsearch")
   result <- readCMSearch (outputDirectory ++ "/" ++ modelType ++ "/" ++ fastaFileName ++ ".cmsearch")
   let rightResults = fromRight result
   let (significantHits,rejectedHits) = partitionCMsearchHitsByScore thresholdScore rightResults
   return (significantHits,rejectedHits)
 
-partitionCMsearchHitsByScore :: Int -> CMsearch -> ([CMsearchHitScore],[CMsearchHitScore])
+partitionCMsearchHitsByScore :: Double -> CMsearch -> ([CMsearchHitScore],[CMsearchHitScore])
 partitionCMsearchHitsByScore thresholdScore cmSearchResult = (selected,rejected)
-  where (selected,rejected) = partition (\hit -> hitScore hit >= fromIntegral thresholdScore) (hitScores cmSearchResult)
+  where (selected,rejected) = partition (\hit -> hitScore hit >= thresholdScore) (hitScores cmSearchResult)
 
 trimCMsearchFastaFile :: String -> String -> String -> CMsearch -> String -> IO ()
 trimCMsearchFastaFile genomesDirectory outputFolder modelType cmsearch fastafile  = do
@@ -83,39 +80,39 @@ trimCMsearchSequence cmSearchResult inputSequence = subSequence
 main :: IO ()
 main = do
   Options{..} <- cmdArgs options  
-  print "RNAlien Statistics:"
   --compute linkscore
   linkscore <- compareCM rfamCovarianceModelPath alienCovarianceModelPath outputDirectoryPath
   rfamMaxLinkScore <- compareCM rfamCovarianceModelPath rfamCovarianceModelPath outputDirectoryPath
   alienMaxLinkscore <- compareCM alienCovarianceModelPath alienCovarianceModelPath outputDirectoryPath
-  putStrLn ("Linkscore: " ++ (show linkscore))
-  putStrLn ("rfamMaxLinkScore: " ++ (show rfamMaxLinkScore))
-  putStrLn ("alienMaxLinkscore: " ++ (show alienMaxLinkscore))
-  
-  --alien vs Rfam max linkscore
-  --rfam vs Rfam max linkscore
-           
-  --other measures
   _ <- system ("cat " ++ rfamFastaFilePath ++ " | grep '>' | wc -l >" ++ outputDirectoryPath ++ FP.takeFileName rfamFastaFilePath ++ ".entries")
   _ <- system ("cat " ++ alienFastaFilePath ++ " | grep '>' | wc -l >" ++ outputDirectoryPath ++ FP.takeFileName alienFastaFilePath ++ ".entries")
   rfamFastaEntries <- readFile (outputDirectoryPath ++ FP.takeFileName rfamFastaFilePath ++ ".entries")
   alienFastaEntries <- readFile (outputDirectoryPath ++ FP.takeFileName alienFastaFilePath ++ ".entries")                    
   let rfamFastaEntriesNumber = read rfamFastaEntries :: Double
   let alienFastaEntriesNumber = read alienFastaEntries :: Double
-    
-  (rfamonAlienResults,rfamonAlienRejected) <- cmSearchFasta rfamThreshold threads rfamCovarianceModelPath outputDirectoryPath "rfamOnAlien" alienFastaFilePath 
-  (alienonRfamResults,alienonRfamRejected) <- cmSearchFasta alienThreshold threads alienCovarianceModelPath outputDirectoryPath "alienOnRfam" rfamFastaFilePath  
+  (rfamonAlienResults,_) <- cmSearchFasta rfamThreshold threads rfamCovarianceModelPath outputDirectoryPath "rfamOnAlien" alienFastaFilePath 
+  (alienonRfamResults,_) <- cmSearchFasta alienThreshold threads alienCovarianceModelPath outputDirectoryPath "alienOnRfam" rfamFastaFilePath  
   let rfamonAlienResultsNumber = fromIntegral (length rfamonAlienResults)
   let alienonRfamResultsNumber = fromIntegral (length alienonRfamResults)
   let rfamonAlienRecovery = rfamonAlienResultsNumber / alienFastaEntriesNumber
   let alienonRfamRecovery = alienonRfamResultsNumber / rfamFastaEntriesNumber
- 
-  putStrLn ("rfamFastaEntriesNumber: " ++ (show rfamFastaEntriesNumber))
-  putStrLn ("alienFastaEntriesNumber: " ++ (show alienFastaEntriesNumber))  
-  putStrLn ("rfamonAlienResultsNumber: " ++ (show rfamonAlienResultsNumber))
-  putStrLn ("alienonRfamResultsNumber: " ++ (show alienonRfamResultsNumber))                         
-  putStrLn ("AlienonRfamRecovery: " ++ (show alienonRfamRecovery))
-  putStrLn ("RfamonAlienRecovery: " ++ (show rfamonAlienRecovery))         
-
-  putStrLn "Done"
-
+  verbose <- getVerbosity  
+  if (verbose == Loud)
+    then do
+       putStrLn ("BenchmarkIndex:" ++ show benchmarkIndex)
+       putStrLn ("RfamModelName: " ++ rfamModelName)
+       putStrLn ("RfamModelId: " ++ rfamModelId)
+       putStrLn ("Linkscore: " ++ show linkscore)
+       putStrLn ("rfamMaxLinkScore: " ++ show rfamMaxLinkScore)
+       putStrLn ("alienMaxLinkscore: " ++ show alienMaxLinkscore)    
+       putStrLn ("rfamGatheringThreshold: " ++ show rfamThreshold)
+       putStrLn ("alienGatheringThreshold: " ++ show alienThreshold) 
+       putStrLn ("rfamFastaEntriesNumber: " ++ show rfamFastaEntriesNumber)
+       putStrLn ("alienFastaEntriesNumber: " ++ show alienFastaEntriesNumber) 
+       putStrLn ("rfamonAlienResultsNumber: " ++ show rfamonAlienResultsNumber)
+       putStrLn ("alienonRfamResultsNumber: " ++ show alienonRfamResultsNumber) 
+       putStrLn ("AlienonRfamRecovery: " ++ show alienonRfamRecovery)
+       putStrLn ("RfamonAlienRecovery: " ++ show rfamonAlienRecovery)
+       putStrLn ("AlienonRfamRecovery: " ++ show alienonRfamRecovery)      
+    else do
+      putStrLn (show benchmarkIndex ++ "\t" ++ rfamModelName ++ "\t" ++ rfamModelId ++ "\t" ++ show linkscore ++ "\t" ++ show rfamMaxLinkScore ++ "\t" ++ show alienMaxLinkscore ++ "\t" ++ show rfamThreshold ++ "\t" ++ show alienThreshold ++ "\t" ++ show rfamFastaEntriesNumber ++ "\t" ++ show alienFastaEntriesNumber ++ "\t" ++ show rfamonAlienResultsNumber ++ "\t" ++ show alienonRfamResultsNumber ++ "\t" ++ show alienonRfamRecovery ++ "\t" ++ show rfamonAlienRecovery ++ "\t" ++ show alienonRfamRecovery)
