e5a9709c0ef4e41dadafaf350ba2ff6e76491321
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-03-24 19:11:20 +0100

    Fixed bug in recoveryplot script

--- scripts/makemultiplotcsv.sh
+++ scripts/makemultiplotcsv.sh
@@ -9,13 +9,13 @@ for i in structuredalienseedoutput4-*.csv; do
 	echo "recoverymultiplot2/rfamonalien/$counter.sorted.csv " >> recoverymultiplot2/filenames
 	echo "$i"
 	cut -d $'\t' -f 13 $i > recoverymultiplot2/rfamonalien/$counter.csv; 
-	cut -d $'\t' -f 13 $i > recoverymultiplot2/alienonrfam/$counter.csv;
+	cut -d $'\t' -f 14 $i > recoverymultiplot2/alienonrfam/$counter.csv;
 	sort -k 1 -n recoverymultiplot2/rfamonalien/$counter.csv > recoverymultiplot2/rfamonalien/$counter.sorted.csv;
 	sort -k 1 -n recoverymultiplot2/alienonrfam/$counter.csv > recoverymultiplot2/alienonrfam/$counter.sorted.csv;
 	counter=$[$counter +1]
 done
 usedfilenames=$(<recoverymultiplot2/filenames)
-echo "usedfilenames"
+echo "$usedfilenames"
 pr -mts $usedfilenames > recoverymultiplot2/rfamonalien/allsorted
 pr -mts $usedfilenames > recoverymultiplot2/alienonrfam/allsorted
 
