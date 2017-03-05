b534f71906c715547f81f588cdaf513376d6e280
Florian Eggenhofer <egg@tbi.univie.ac.at>
2017-03-05 18:07:09 +0100

    Added setup check and improved temp dir handling

--- src/Bio/RNAlien.hs
+++ src/Bio/RNAlien.hs
@@ -34,13 +34,14 @@ data Options = Options
     threads :: Int,
     taxonomyRestriction :: Maybe String,
     sessionIdentificator :: Maybe String,
-    performEvaluation :: Bool
+    performEvaluation :: Bool,
+    checkSetup :: Bool
   } deriving (Show,Data,Typeable)
 
 options :: Options
 options = Options
   { inputFastaFilePath = def &= name "i" &= help "Path to input fasta file",                       
-    outputPath = def &= name "o" &= help "Path to output directory",
+    outputPath = def &= name "o" &= help "Path to output directory. Default: current working directory",
     inputTaxId = Nothing &= name "t" &= help "NCBI taxonomy ID number of input RNA organism",
     inputnSCICutoff = (Just (1 :: Double)) &= name "z" &= help "Only candidate sequences with a normalized structure conservation index (nSCI) higher than this value are accepted. Default: 1",
     inputEvalueCutoff = (Just (0.001 :: Double)) &= name "e" &= help "Evalue cutoff for cmsearch filtering. Default: 0.001",
@@ -54,64 +55,74 @@ options = Options
     threads = 1 &= name "c" &= help "Number of available cpu slots/cores. Default: 1",
     taxonomyRestriction = Nothing &= name "r" &= help "Restrict search space to taxonomic kingdom (bacteria,archea,eukaryia). Default: not set",
     sessionIdentificator = Nothing &= name "d" &= help "Optional session id that is used instead of automatically generated one.",
-    performEvaluation = True &= name "x" &= help "Perform evaluation step. Default: True"
+    performEvaluation = True &= name "x" &= help "Perform evaluation step. Default: True",
+    checkSetup = False &= name "g" &= help "Just prints installed tool versions and performs connection check. Default: False"
   } &= summary ("RNAlien " ++ alienVersion) &= help "Florian Eggenhofer, Ivo L. Hofacker, Christian Hoener zu Siederdissen - 2013 - 2017" &= verbosity       
                 
 main :: IO ()
 main = do
   Options{..} <- cmdArgs options
   verboseLevel <- getVerbosity
+  let tools = if inputQuerySelectionMethod == "clustering" then ["clustalo","mlocarna","RNAfold","RNAalifold","cmcalibrate","cmstat","cmbuild","RNAz","RNAcode"] else ["mlocarna","RNAfold","RNAalifold","cmcalibrate","cmstat","cmbuild","RNAz","RNAcode"]
   -- Generate SessionID
   sessionId <- createSessionID sessionIdentificator
   timestamp <- getCurrentTime
-  let temporaryDirectoryPath = FP.addTrailingPathSeparator outputPath ++ sessionId ++ "/"            
+  currentWorkDirectory <- getCurrentDirectory
+  let selectedOutputPath = if null outputPath then currentWorkDirectory else outputPath
+  let temporaryDirectoryPath = FP.addTrailingPathSeparator selectedOutputPath ++ sessionId ++ "/"            
   createDirectoryIfMissing False temporaryDirectoryPath
-  createDirectoryIfMissing False (temporaryDirectoryPath ++ "log")
-  -- Create Log files
-  writeFile (temporaryDirectoryPath ++ "Log") ("RNAlien " ++ alienVersion ++ "\n")
-  writeFile (temporaryDirectoryPath ++ "log/warnings") ("")
-  logMessage ("Timestamp: " ++ (show timestamp) ++ "\n") temporaryDirectoryPath
-  logMessage ("Temporary Directory: " ++ temporaryDirectoryPath ++ "\n") temporaryDirectoryPath
-  inputFasta <- readFasta inputFastaFilePath
   networkCheck <- checkNCBIConnection
-  if isLeft networkCheck
+  if checkSetup
     then do
-      putStrLn ("Error - Could not contact NCBI server: " ++ fromLeft networkCheck ++ "\n")
-      logMessage ("Error - Could not contact NCBI server: " ++ fromLeft networkCheck ++ "\n") temporaryDirectoryPath
+      toolsCheck <- checkTools tools inputQuerySelectionMethod temporaryDirectoryPath
+      let setupCheckPath = temporaryDirectoryPath ++ "setupCheck"
+      let toolCheckResult = either id id toolsCheck
+      let networkCheckResult = either id id networkCheck
+      writeFile setupCheckPath (toolCheckResult ++ "\n" ++ networkCheckResult ++ "\n")
     else do
-      if null inputFasta
+      if isLeft networkCheck
         then do
-          putStrLn "Error: Input fasta file is empty."
-          logMessage "Error: Input fasta file is empty.\n" temporaryDirectoryPath
-        else do
-          let iterationNumber = 0
-          let tools = if inputQuerySelectionMethod == "clustering" then ["clustalo","mlocarna","RNAfold","RNAalifold","cmcalibrate","cmstat","cmbuild","RNAz","RNAcode"] else ["mlocarna","RNAfold","RNAalifold","cmcalibrate","cmstat","cmbuild","RNAz","RNAcode"]
-          toolsCheck <- checkTools tools inputQuerySelectionMethod temporaryDirectoryPath
-          -- Check required commandline tools
-          if isLeft toolsCheck
-            then do 
-              putStrLn ("Error - Not all required tools could be found in $PATH: " ++ fromLeft toolsCheck ++ "\n")
-              logMessage ("Error - Not all required tools could be found in $PATH: " ++ fromLeft toolsCheck ++ "\n") temporaryDirectoryPath
-            else do
-              logToolVersions inputQuerySelectionMethod temporaryDirectoryPath
-              let inputSequence = reformatFasta (head inputFasta)
-              initialTaxId <- setInitialTaxId inputBlastDatabase temporaryDirectoryPath inputTaxId inputSequence
-              let checkedTaxonomyRestriction = checkTaxonomyRestriction taxonomyRestriction
-              let staticOptions = StaticOptions temporaryDirectoryPath sessionId (fromJust inputnSCICutoff) inputTaxId singleHitperTax inputQuerySelectionMethod inputQueryNumber lengthFilter coverageFilter blastSoftmasking threads inputBlastDatabase checkedTaxonomyRestriction (setVerbose verboseLevel)
-              let initialization = ModelConstruction iterationNumber inputSequence [] initialTaxId Nothing (fromJust inputEvalueCutoff) False [] []
-              logMessage (show initialization) temporaryDirectoryPath
-              modelConstructionResults <- modelConstructer staticOptions initialization
-              let resultTaxonomyRecordsCSVTable = constructTaxonomyRecordsCSVTable modelConstructionResults
-              writeFile (temporaryDirectoryPath ++ "result.csv") resultTaxonomyRecordsCSVTable
-              if performEvaluation
-                then do
-                  resultEvaluation <- evaluateConstructionResult staticOptions modelConstructionResults
-                  appendFile (temporaryDirectoryPath ++ "Log") resultEvaluation
-                  resultSummary modelConstructionResults staticOptions
-                  writeFile (temporaryDirectoryPath ++ "done") ""
-                else do
-                  resultSummary modelConstructionResults staticOptions
-                  writeFile (temporaryDirectoryPath ++ "done") ""
+          putStrLn ("Error - Could not contact NCBI server: " ++ fromLeft networkCheck ++ "\n")
+          logMessage ("Error - Could not contact NCBI server: " ++ fromLeft networkCheck ++ "\n") temporaryDirectoryPath
+       else do
+           createDirectoryIfMissing False (temporaryDirectoryPath ++ "log")
+           -- Create Log files
+           writeFile (temporaryDirectoryPath ++ "Log") ("RNAlien " ++ alienVersion ++ "\n")
+           writeFile (temporaryDirectoryPath ++ "log/warnings") ("")
+           logMessage ("Timestamp: " ++ (show timestamp) ++ "\n") temporaryDirectoryPath
+           logMessage ("Temporary Directory: " ++ temporaryDirectoryPath ++ "\n") temporaryDirectoryPath
+           inputFasta <- readFasta inputFastaFilePath
+           if null inputFasta
+             then do
+               putStrLn "Error: Input fasta file is empty."
+               logMessage "Error: Input fasta file is empty.\n" temporaryDirectoryPath
+             else do
+               let iterationNumber = 0        
+               toolsCheck <- checkTools tools inputQuerySelectionMethod temporaryDirectoryPath
+               if isLeft toolsCheck
+                 then do 
+                   putStrLn ("Error - Not all required tools could be found in $PATH: " ++ fromLeft toolsCheck ++ "\n")
+                   logMessage ("Error - Not all required tools could be found in $PATH: " ++ fromLeft toolsCheck ++ "\n") temporaryDirectoryPath
+                 else do
+                   logToolVersions inputQuerySelectionMethod temporaryDirectoryPath
+                   let inputSequence = reformatFasta (head inputFasta)
+                   initialTaxId <- setInitialTaxId inputBlastDatabase temporaryDirectoryPath inputTaxId inputSequence
+                   let checkedTaxonomyRestriction = checkTaxonomyRestriction taxonomyRestriction
+                   let staticOptions = StaticOptions temporaryDirectoryPath sessionId (fromJust inputnSCICutoff) inputTaxId singleHitperTax inputQuerySelectionMethod inputQueryNumber lengthFilter coverageFilter blastSoftmasking threads inputBlastDatabase checkedTaxonomyRestriction (setVerbose verboseLevel)
+                   let initialization = ModelConstruction iterationNumber inputSequence [] initialTaxId Nothing (fromJust inputEvalueCutoff) False [] []
+                   logMessage (show initialization) temporaryDirectoryPath
+                   modelConstructionResults <- modelConstructer staticOptions initialization
+                   let resultTaxonomyRecordsCSVTable = constructTaxonomyRecordsCSVTable modelConstructionResults
+                   writeFile (temporaryDirectoryPath ++ "result.csv") resultTaxonomyRecordsCSVTable
+                   if performEvaluation
+                     then do
+                       resultEvaluation <- evaluateConstructionResult staticOptions modelConstructionResults
+                       appendFile (temporaryDirectoryPath ++ "Log") resultEvaluation
+                       resultSummary modelConstructionResults staticOptions
+                       writeFile (temporaryDirectoryPath ++ "done") ""
+                     else do
+                       resultSummary modelConstructionResults staticOptions
+                       writeFile (temporaryDirectoryPath ++ "done") ""
 
 alienVersion :: String
 alienVersion = showVersion version
